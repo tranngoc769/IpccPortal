01bfbf98f8ceedbc64a81f73842a8f1da61b6f28
Quang <hocmai6@gmail.com>
2020-10-29 11:41:32 +0700

    layout callpad

--- app.js
+++ app.js
@@ -4,49 +4,56 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const session = require('express-session');
-var app = express();
-app.use(session({
-    secret: 'tel4vn',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true, maxAge: 60000 }
-}))
-const middleware = require('./util/middleware/middleware')
 var bodyParser = require("body-parser");
-/* @@END VIEWS ENGINE */
+var app = express();
+var session = require('express-session')
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
+})
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+        secret: 'tel4vn',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true }
+    }))
+    /* @@END VIEWS ENGINE */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/users', middleware.auth, usersRouter);
-
+/* BODY_PARSER */
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: '50mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '60mb', extended: true }))
+const middleware = require('./middleware/middleware');
+var contact = require('./routes/contact');
+var contactapi = require('./routes/contactapi');
+app.use('/', cookieParser(), indexRouter);
+app.use('/contact', cookieParser(), contact);
+// app.use('/users', middleware.auth, usersRouter);
+// API
+app.use('/api/contact', cookieParser(), contactapi);
+// app.use('/api/contact', cookieParser(), middleware.permission, contactapi);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-    console.log(err)
-        // render the error page
+    // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
-
-/* BODY_PARSER */
-app.use(bodyParser.json({ limit: '50mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '60mb', extended: true }))
-    /* END @@BODY_PARSER */
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log('App listening on port: ' + port);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,49 @@
 var express = require('express');
+const session = require('express-session');
 var router = express.Router();
+const userModel = require('../models/user')
+const contactModel = require('../models/contact')
+const auth = require('../util/auth')
+const middleware = require('../middleware/middleware')
+const md5 = require('md5')
 
-const middleware = require('../util/middleware/middleware')
-    /* GET home page. */
-router.get('/', middleware.auth, function(req, res, next) {
+
+// 
+/* GET home page. */
+router.get('/', middleware.auth, async function(req, res, next) {
+
+    // Get list contact
+    var totalContact = await contactModel.getAllContact();
+    console.log(totalContact)
     res.render('index', {
-        title: 'Index',
-        mess: 'Login success'
+        totalContact: totalContact.length,
+        allContact: totalContact
     });
 });
 router.get('/signin', function(req, res, next) {
     res.render('signin', { title: 'Express' });
 });
-router.post('/signin', function(req, res, next) {
-    // logic here
-    console.log("Test");
+router.post('/signin', async function(req, res, next) {
     var bodydata = req.body;
-    console.log(bodydata);
-    req.session.User = {
-        username: 'tranngoc',
-        something: 'something'
+    var username = bodydata.username;
+    var password = bodydata.password;
+    var userRow = await userModel.getUserByName(username)
+    if (userRow.length < 1) {
+        return res.redirect('/signin');
     }
-    res.redirect('/');
-})
+    var data = userRow[0];
+    const payload = {
+        username: data.username,
+        email: data.email,
+
+    };
+    var hash_pass = md5(password)
+    if (hash_pass == data.password) {
+        const token = await auth.generateToken(payload);
+        res.cookie('jwt', token);
+        return res.redirect('/');
+    } else {
+        return res.redirect('/signin');
+    }
+});
 module.exports = router;
